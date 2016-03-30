79ffdb52efc9b0b4d6dbe14b2c83395abed98542
carlynorama <carlynorama@gmail.com>
2016-03-30 13:12:13 -0700

    interesting split generated... why?

--- MonsteraDeliciosa/sketch.js
+++ MonsteraDeliciosa/sketch.js
@@ -1,12 +1,12 @@
 var r = new Rune({
   container: "#canvas",
   width: 800,
-  height: 2000,
+  height: 3000,
   debug: true
 });
 
 var voronoi = new Voronoi();
-var bbox = {xl: 0, xr: 800, yt: 0, yb: 2000}; // xl is x-left, xr is x-right, yt is y-top, and yb is y-bottom
+var bbox = {xl: 0, xr: 800, yt: 0, yb: 3000}; // xl is x-left, xr is x-right, yt is y-top, and yb is y-bottom
 var sites = [ {x: 200, y: 200}, {x: 50, y: 250}, {x: 300, y: 100} /* , ... */ ];
 var margin = 13;
 var diagram = voronoi.compute(sites, bbox);
@@ -17,7 +17,7 @@ var diagram = voronoi.compute(sites, bbox);
 // around 6 accross until very end
 // everything gets bigger as it goes up
 
-monsteraDeliciosaFruitSites2(5, 10);
+monsteraDeliciosaFruitSites2(2, 10);
 
   for (slocs of sites) {
    //console.log(slocs);
@@ -56,38 +56,6 @@ monsteraDeliciosaFruitSites2(5, 10);
   		diagram = voronoi.compute(sites, bbox);
   }
 
-
-  //  function monsteraDeliciosaFruitSites(base, fuzz) {
-   //
-  //    var xlocation, ylocation;
-  //    var xspace = base;
-  //    var yspace = base * 9/10;
-  //    var xoffset = base/2;
-  //    var voffset = 0.9;
-  //    var localfuzz = fuzz;
-   //
-  //    var cellswide = 6;
-  //    var cellshigh = ((r.height/yspace)+2);
-   //
-  //    var vmidline = 0.5 * cellswide;
-  //    var hmidline = 0.5 * cellshigh;
-   //
-  //    sites = [];
-  //    for(var i = 0; i < cellswide; i++) {
-  //     for(var j = 0; j < cellshigh; j++) {
-  //        xlocation = i*(xspace-j*voffset) + Rune.random(localfuzz);
-  //        ylocation = j*(yspace-j*voffset) + Rune.random(localfuzz);
-  //        //console.log(xlocation);
-  //        //console.log(ylocation);
-  //        if (j%2==0) {
-  //          xlocation +=xoffset;
-  //        }
-  //        sites.push({x:xlocation, y:ylocation});
-  //       }
-  //     }
-  //    diagram = voronoi.compute(sites, bbox);
-  //  }
-
    function monsteraDeliciosaFruitSites2(base, fuzz) {
 
      var aspectratio = 1.3
@@ -104,8 +72,8 @@ monsteraDeliciosaFruitSites2(5, 10);
      // around 6 accross until very end
      // everything gets bigger as it goes up
 
-     var cellswide = 6;
-     var cellshigh = 36;
+     var cellswide = 10;
+     var cellshigh = 80;
      var topexpansionslope = 1/aspectratio;//cellswide/8;
      var vmidline = 0.5 * cellswide;
      var hmidline = 0.5 * cellshigh;
@@ -113,10 +81,12 @@ monsteraDeliciosaFruitSites2(5, 10);
      sites = [];
 
       for(var j = 1; j < cellshigh+1; j++) {
-        cellsthisrow = Math.min(j*topexpansionslope, cellswide);
+        cellsthisrow = cellswide; //Math.min(j*topexpansionslope, cellswide);
         for(var i = 0; i < cellsthisrow; i++) {
-          var  rowspacing = xspace-j*voffset;
-          var  colspacing = yspace-j*voffset;
+          var Q = cellshigh/(j*aspectratio);
+           console.log(Q);
+          var  rowspacing = Q*(xspace-j*voffset);
+          var  colspacing = Q*(yspace-j*voffset);
          xlocation = i*(rowspacing);//+ Rune.random(localfuzz);
          ylocation = j*(colspacing);//*(1+Rune.random(1+localfuzz/100));
          //console.log(xlocation);
